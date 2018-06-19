bcc9e2cd83ea2e207af99a3a42badfaed92d0aa9
Pascal Knoth <pknoth@octo.com>
2018-06-19 15:39:08 +0200

    removes whitespace

--- kong/plugins/customAuth/handler.lua
+++ kong/plugins/customAuth/handler.lua
@@ -24,7 +24,7 @@ function plugin:access(conf)
     local res, err = httpc:request_uri(plugin:authorize_url(conf), {
       method = "GET",
       path = conf.authorize_path,
-      headers =  headers
+      headers = headers
     })
 
     if not res then
